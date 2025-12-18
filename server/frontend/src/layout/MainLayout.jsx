6cd89871519234149113a08a31bd452b2216b76e
Max Luecke <max@plainskill.net>
2025-12-17 19:53:23 -0800

    Switch from flex-grow to simply using grow className

--- server/frontend/src/layout/MainLayout.jsx
+++ server/frontend/src/layout/MainLayout.jsx
@@ -1,13 +1,13 @@
-import { Outlet } from 'react-router-dom';
-import Navbar from '../components/common/Navbar';
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/common/Navbar";
 
 export default function MainLayout() {
-    return (
-        <div className="relative flex flex-col">
-            <main className='flex-grow w-full'>
-                <Outlet/>
-            </main>
-            <Navbar/>
-        </div>
-    )
+  return (
+    <div className="relative flex flex-col">
+      <main className="grow w-full">
+        <Outlet />
+      </main>
+      <Navbar />
+    </div>
+  );
 }
