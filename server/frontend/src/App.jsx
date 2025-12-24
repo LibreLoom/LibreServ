001f4b41eb5f566a5085940104d3d084bb43a1cd
Max Luecke <max@plainskill.net>
2025-12-24 13:33:07 -0800

    Implement LoginPage Route

--- server/frontend/src/App.jsx
+++ server/frontend/src/App.jsx
@@ -1,22 +1,24 @@
-import './App.css';
-import MainLayout from './layout/MainLayout';
-import DashboardPage from './pages/DashboardPage';
-import AppsPage from './pages/AppsPage';
-import UsersPage from './pages/UsersPage';
-import SettingsPage from './pages/SettingsPage';
-import HelpPage from './pages/HelpPage';
-import { Routes, Route } from 'react-router-dom';
+import "./App.css";
+import MainLayout from "./layout/MainLayout";
+import DashboardPage from "./pages/DashboardPage";
+import AppsPage from "./pages/AppsPage";
+import UsersPage from "./pages/UsersPage";
+import SettingsPage from "./pages/SettingsPage";
+import HelpPage from "./pages/HelpPage";
+import Login from "./pages/Login";
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <Routes>
-      <Route element={<MainLayout/>}>
-        <Route path="/" element={<DashboardPage/>}/>
-        <Route path="/apps" element={<AppsPage/>}/>
-        <Route path="/users" element={<UsersPage/>}/>
-        <Route path="/settings" element={<SettingsPage/>}/>
-        <Route path="/help" element={<HelpPage/>}/>
+      <Route path="/login" element={<Login />} />
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/apps" element={<AppsPage />} />
+        <Route path="/users" element={<UsersPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/help" element={<HelpPage />} />
       </Route>
     </Routes>
-  )
+  );
 }
